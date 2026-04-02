a73bdf229cc2e16fe5c9bcad7ed6ade12aba4226
AdityaCS025 <adityaworld2006@gmail.com>
2026-04-02 13:22:18 +0530

    Relax password policy

--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -16,9 +16,7 @@ export const validateEmail = (): ValidationChain => {
 const passwordPolicy = (field: string): ValidationChain => {
     return body(field)
         .isLength({ min: 8 })
-        .withMessage('Password must be at least 8 characters long')
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)
-        .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character');
+        .withMessage('Password must be at least 8 characters long');
 };
 
 export const validatePassword = (): ValidationChain => {
